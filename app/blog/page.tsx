9076d09e6fa46fb1f3147a1a21a26d5a45d930c3
nerots123 <hamdanubaidillah0306@gmail.com>
2023-06-18 14:18:34 +0700

    change blog font size

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -8,7 +8,7 @@ export default function Blog() {
   return (
     <div className="main-layout h-[80vh] flex-center">
       <div className="bg-slate-200 dark:bg-secondary rounded-[10px] sm:p-20 p-10">
-        <h1 className="text-2xl font-semibold text-primary dark:text-white">
+        <h1 className="sm:text-2xl text-xl font-semibold text-primary dark:text-white">
           🔨 Still on project! 🔧
         </h1>
       </div>
